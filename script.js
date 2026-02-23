6b253c0b3de28ae6c8e0c29f3397e5bcef19b9ab
abi4679553 <abivengadajalam7708@gmail.com>
2026-02-23 20:57:28 +0530

    logics

--- script.js
+++ script.js
@@ -1,2 +1,41 @@
 
 // console.log("hejkwn")
+
+// var a = "saravanan"
+// console.log(a)
+
+// var b = "abi"
+// var c = "saro"
+// d = b*c ;
+// console.log(d)
+
+// let f = "kavin"
+// f = "priya"
+// console.log(f)
+
+// const g = 20;
+
+// console.log(g)
+
+// var a = 10;
+// var b = 30;
+// c = a+b;
+// console.log(c)
+
+// var g = 12;
+// if (g>=18)
+// {
+//     console.log("voted")
+// }
+// else{
+//     console.log("not voted")
+// }
+
+let a = 10;
+let b = 20;
+let abi = a;
+a = b;
+b = abi;
+console.log("a",a)
+console.log("b",b)
+
