2d08922660d8430885112fdedbf1393956eb8f06
abi4679553 <abivengadajalam7708@gmail.com>
2026-03-30 13:48:02 +0530

    Switch  statement

---
+++ ControlStatements/Switch.js
@@ -0,0 +1,11 @@
+let day = 1;
+switch(day){
+    case 1 :
+        console.log("tuesday")
+        break;
+    case 2 :
+        console.log("monday")
+        break;
+        default:
+            console.log("another day")
+}
