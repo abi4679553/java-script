17362d9f0d15420121ca62ab368ff1dd08e9a0f8
abi4679553 <abivengadajalam7708@gmail.com>
2026-03-31 12:19:15 +0530

    forloop

---
+++ Looping/for.js
@@ -0,0 +1,48 @@
+for(let i = 0; i <= 5; i++){
+    console.log(i)   
+}
+
+for(var a = 5; a >= 1; a--){
+    console.log(a)
+}
+
+let store = "";
+for (let i = 1; i <= 5; i++) {
+    store = store + i;
+}
+console.log(store)
+
+let store = "";
+for (let i = 20; i<=30; i--) {
+    store = store + "i";
+    console.log(store);
+}
+
+
+
+let store = "";
+for(let i = 1;i <= 5;i++){
+    store = store + "1";
+    console.log(store)
+}
+
+let store = "";
+for(let a = 1; a <= 5; a++){
+    store = store + a;
+    console.log(store)
+}
+
+let store = "";
+for(let i = 5;i >= 1;i--){
+    store = store + i;
+    console.log(store)
+}
+
+
+// for(i=1;i<=5;1++){
+//     let resut = ""
+//     for(j=1;j<=5;j++){
+//         resut = resut +"i"
+//         console.log(resut)
+//     }
+// }
